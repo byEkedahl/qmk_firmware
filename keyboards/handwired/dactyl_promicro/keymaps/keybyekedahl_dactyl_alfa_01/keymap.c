6b4231983509adf2487e4c3376ac26c545e30dfd
fredrik <fredrik@flygresor.se>
2019-05-29 18:35:17 +0200

    layout change

--- keyboards/handwired/dactyl_promicro/keymaps/keybyekedahl_dactyl_alfa_01/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/keybyekedahl_dactyl_alfa_01/keymap.c
@@ -11,13 +11,36 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* Keymap 0: _QWERTY
+ *
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | =/L0 | 1/L1 | 2/L2 |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | CAPS |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |; / L2|'/Cmd |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|Z/Ctrl|   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |//Ctrl|RShift|
+ * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
+ * |Debug |  '"  |AltShf| Left | Right|                                  |  Up  | Down |   [  |   ]  | ~L1  |
+ * `----------------------------------'                                  `----------------------------------'
+ *                                      ,-------------.  ,-------------.
+ *                                      | LGui | LAlt |  | RAlt | LGui |
+ *                               ,------|------|------|  |------+------+------.
+ *                               |      |      | Home |  | PgUp |      |      |
+ *                               |Back  |Space |------|  |------|  Tab |Enter |
+ *                               |Space |      | End  |  | PgDn |      |      |
+ *                               `--------------------'  `--------------------'
+ */
+
+
 [_QWERTY] = LAYOUT_6x6(
-	LT(0,KC_EQL),	LT(1,KC_1), LT(2,KC_2),		KC_3	,		KC_4	,		KC_5,							KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
-		KC_TAB		, 	KC_Q		,		KC_W		,		KC_E	,		KC_R	,		KC_T,							KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
-		KC_CAPS		,		KC_A		,		KC_S		,		KC_D	,		KC_F	, 	KC_G,							KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-		KC_LSFT		,		KC_Z		,		KC_X		,		KC_C	,		KC_V	,		KC_B,							KC_N, KC_M, KC_COMM, KC_DOT, KC_MINS, KC_RSFT,
-		DEBUG			,	KC_COMM		,		KC_GRV		,	KC_LEFT	,	KC_RGHT	,	LGUI_T(KC_ESC),		KC_UP, KC_DOWN, KC_QUOT, KC_RBRC, KC_MSTP,
-								KC_LALT		,	LCTL_T(KC_HOME),	KC_BSPC	, KC_DEL	, KC_END,				KC_RALT, RGUI_T(KC__MUTE), KC_PGUP, KC_PGDN, KC_ENT, KC_SPC),
+	LT(0,KC_EQL),	LT(1,KC_1), LT(2,KC_2)    ,		KC_3	,     KC_4     , KC_5  ,							KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
+		KC_TAB		, 	KC_Q		,		KC_W		    ,		KC_E	,     KC_R     , KC_T  ,							KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
+		KC_CAPS		,		KC_A		,		KC_S		    ,		KC_D	,		  KC_F     , KC_G  ,							KC_H   , KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+		KC_LSFT		,		KC_Z		,		KC_X		    ,		KC_C	,		  KC_V     , KC_B  ,							KC_N   , KC_M, KC_COMM, KC_DOT, KC_MINS, KC_RSFT,
+		DEBUG			,	KC_COMM		,		KC_GRV		  ,	KC_LEFT	,   KC_RGHT    ,KC_BSPC,		          KC_SPC , KC_UP, KC_DOWN, KC_QUOT, KC_RBRC, KC_MSTP,
+    KC_DEL    ,  KC_END   ,LCTL_T(KC_HOME), KC_LALT	,LGUI_T(KC_ESC),				              RGUI_T(KC__MUTE), KC_RALT, KC_PGUP, KC_PGDN, KC_ENT),
 
 
 [_LOWER] = LAYOUT_6x6(
